a94a9bd29ee270fa3af69089300c6490895e3799
Pramila Shrestha <pramilashr7@gmail.com>
2019-01-04 09:21:52 +0545

    added new.playground

---
+++ new.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+
+var age = 13
+
+if age > 18{
+    print("You are \(age) years old")
+}else{
+    print("Error you are \(age)")
+}
+
+
+var number = 13
+func eligible(age: Int) -> Bool {
+    if number >= age {
+        print("you are \(age)")
+        return true
+    }
+    else{
+        print("Error")
+        return false
+    }
+}
